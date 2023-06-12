8d2a4b33c686b9af4398c1fe595fbed0e1a20a39
kaustubhvasagadekar <kaustubh111111@gmail.com>
2023-06-13 02:54:08 +0530

    basic commit on about page and added image.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	const year = new Date().getFullYear();
 	return (
 		<>
-			<footer className="text-center text-capitalize">
+			<footer className="text-center text-capitalize py-3 pt-5 mt-5">
 				copyright &copy; {year}
 			</footer>
 		</>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,41 @@
-import Header from '../components/Header'
-import Meta from '../components/Meta'
+import Header from "../components/Header";
+import Meta from "../components/Meta";
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import Image from "react-bootstrap/Image";
+import College from "../assets/college-1.jpeg";
+import { Container } from "react-bootstrap";
 
 const About = () => {
   // page content
-  const pageTitle = 'About'
-  const pageDescription = ''
+  const PageTitle1 = "About GNI";
+  const Description1 =
+    "Guru Nanak Institutions (GNI) is a multi-disciplinary group of institutions situated within 30 minutes drive from Hyderabad International Airport, in the Greater Hyderabad Municipal Corporation limits. GNI offers various Faculties of studies viz, Engineering, Management Studies and Pharmacy. GNI is a dream venture of Guru Nanak Educational Society (GNES) having an excellent track record of 22 years in delivering quality higher education in the fields of Engineering & Technology, Pharmacy, Management and Dental Sciences.";
+
+  const PageTitle2 = "About HACKTOPIA";
+  const Description2 =
+    "Hacktopia is a Hackathon event being conduct first time in GNI Campus under the Aegis of Department of Computer Science & Engineering The term hackathon is derived from the combination of the phrases hack and  marathon. In its most basic form, a hackathon is a collaborative problem-solving challenge. The goal is to encourage innovation and showcase fresh ideas or solutions.";
 
   return (
     <div>
-      <Meta title={pageTitle}/>
-      <Header head={pageTitle} description={pageDescription} />
-    </div>
-  )
-}
+      <Meta title={PageTitle1} />
+      <Header head={PageTitle1} description={Description1} />
+      <Container className="aline-content-center">
+        <Image className="py-3  " src={College} rounded />
+      </Container>
+      <Header head={PageTitle2} description={Description2} />
 
-export default About
+      {/* <Row>
+        <Col sm={8}>
+      <Meta title={PageTitle1} />
+      <Header head={PageTitle1} description={Description1} />
+      <Header head={PageTitle2} description={Description2} /></Col>
+        <Col sm={4}>
+        <Image className="" src={College} rounded /> 
+        </Col>
+      </Row> */}
+    </div>
+  );
+};
+
+export default About;
